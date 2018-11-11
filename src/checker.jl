2662d76a5c73abcfc5c4e20dd0707921af75db31
alstat <alasaadstat@gmail.com>
2018-11-12 00:10:25 +0800

    formatted properly

--- src/checker.jl
+++ src/checker.jl
@@ -7,7 +7,7 @@ function checker(::Type{T}, d::AbstractDataFrame, args::Symbol...) where T <: Ab
     return length(args) != 3 ? error(err1) : sum([args[i] for i in 1:length(args)] .∈ [names(d)]) != length(args) ? error(err2) : true
 end
 
-function checker(::Type{AbstractTest}, d::AbstractDataFrame, args::Symbol...)
+function checker(::Type{T}, d::AbstractDataFrame, args::Symbol...) where T <: AbstractTest
     err1 = "args... takes two arguments for the following variables (in order): aberration, cells"
     err2 = "args... should match the corresponding column name in d"
     return length(args) != 2 ? error(err1) : sum([args[i] for i in 1:length(args)] .∈ [names(d)]) != length(args) ? error(err2) : true
