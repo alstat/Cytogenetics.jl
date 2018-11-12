96ddbb5b51d241fb895aa7344e10342483090822
alstat <alasaadstat@gmail.com>
2018-11-13 00:28:54 +0800

    minor updates to changes

--- test/cobalt60.jl
+++ test/cobalt60.jl
@@ -7,12 +7,12 @@ using CSV
 data = DataFrame(CSV.File(joinpath(homedir(), "Documents", "Cobalt60.csv")));
 data
 
-x = test(AbstractUTest, data, :aberr, :cell; celldist = [4, 9])
+x = test(UTest, data, :aberr, :cell; celldist = [4, 9])
 
-fit(LinearQuadratic, data, :aberr, :cell, :doses)
+fit(LinearQuadratic, x, :aberr, :cell, :doses)
 fit(LinearQuadratic(true), data, :aberr, :cell, :doses)
 fit(LinearQuadratic(overdispersed = true), data, :aberr, :cell, :doses)
 
-fit(Linear, data, :aberr, :cell, :doses)
+fit(Linear, x, :aberr, :cell, :doses)
 fit(Linear(overdispersed = true), data, :aberr, :cell, :doses)
 fit(Linear(true), data, :aberr, :cell, :doses)
