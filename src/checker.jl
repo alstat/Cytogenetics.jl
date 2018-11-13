ea796dd9f6f5554fb340f68ccc5e4842cee44e08
alstat <alasaadstat@gmail.com>
2018-11-14 00:00:34 +0800

    updated variable name and added methods for instance objects

--- src/checker.jl
+++ src/checker.jl
@@ -1,14 +1,17 @@
 """
 Checker Functions
 """
-function checker(::Type{T}, d::AbstractDataFrame, args::Symbol...) where T <: AbstractModel
+function checker(::Type{T}, df::DataFrame, args::Symbol...) where T <: AbstractModel
     err1 = "args... takes three arguments for the following variables (in order): aberration, cells, doses"
     err2 = "args... should match the corresponding column name in d"
-    return length(args) != 3 ? error(err1) : sum([args[i] for i in 1:length(args)] .∈ [names(d)]) != length(args) ? error(err2) : true
+    return length(args) != 3 ? error(err1) : sum([args[i] for i in 1:length(args)] .∈ [names(df)]) != length(args) ? error(err2) : true
 end
 
-function checker(::Type{T}, d::AbstractDataFrame, args::Symbol...) where T <: AbstractTest
+function checker(::Type{T}, df::DataFrame, args::Symbol...) where T <: AbstractTest
     err1 = "args... takes two arguments for the following variables (in order): aberration, cells"
     err2 = "args... should match the corresponding column name in d"
-    return length(args) != 2 ? error(err1) : sum([args[i] for i in 1:length(args)] .∈ [names(d)]) != length(args) ? error(err2) : true
+    return length(args) != 2 ? error(err1) : sum([args[i] for i in 1:length(args)] .∈ [names(df)]) != length(args) ? error(err2) : true
 end
+
+checker(M::LinearQuadratic, df::DataFrame, args::Symbol...) = checker(LinearQuadratic, df, args...)
+checker(M::Linear, df::DataFrame, args::Symbol...) = checker(Linear, df, args...)
